72273c2d5d3bf7be480086fbda6446ca75366dc1
roniekun <roniebenitez01@gmail.com>
2024-02-02 02:58:39 +0800

    update

--- src/pages/home/assets/Button.jsx
+++ src/pages/home/assets/Button.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Gmail from '../../../assets/Gmail';
 import { useContext } from 'react';
 import { DataContext } from '../../../context/DataContext';
-import MessageIcon from './svg/send-1.svg?react'
+// import MessageIcon from './svg/send-1.svg?react'
+import { TbSend } from "react-icons/tb";
 
 const Button = () => {
   const { user } = useContext(DataContext)
@@ -13,8 +14,8 @@ const Button = () => {
   return (
     <button
       onClick={handleClick}
-      className="flex items-center justify-center primary-font uppercase text-xs gap-2  hover:scale-105 transition transform text-black active:bg-green-600 focus:outline-none hover:shadow-lg hover:bg-green-400 focus:ring  focus:ring-offset-1 font-medium  focus:ring-gray-300 border border-gray-900 rounded-md py-2 px-5">
-    <MessageIcon className='w-6'/>  Send Message
+      className="group flex items-center justify-center primary-font uppercase text-xs gap-2  hover:scale-105 transition transform text-black hover:text-white hover:bg-gray-800 focus:outline-none hover:shadow-lg hover7text-white focus:ring  focus:ring-offset-1 font-medium  focus:ring-gray-300 border border-gray-900 rounded-md py-2 px-5">
+    <TbSend className='w-3 group-hover:stroke-white'/>  Send Message
     </button>
   );
 }
