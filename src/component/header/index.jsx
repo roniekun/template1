57ff19ba93ef1a206aee3663d3692428dbec69c4
roniekun <roniebenitez01@gmail.com>
2024-02-01 22:54:23 +0800

    update

--- src/component/header/index.jsx
+++ src/component/header/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './style.css'
 import Menu from './assets/Menu'
 import Home from './assets/Home'
 import { useLocation } from 'react-router-dom'
@@ -8,6 +7,7 @@ import { DataContext } from '../../context/DataContext'
 import Logo from './assets/Logo'
 import Nav from './assets/Nav'
 import Socials from './assets/Socials'
+import { motion } from 'framer-motion'
 
 const Header = () => {
   const [homeButton, setHomeButton] = useState(false)
@@ -18,15 +18,16 @@ const Header = () => {
     setHomeButton(location.pathname !== '/' ? true : false)
   }, [location])
 
-  console.log(isMobile)
-
   return (
-    <main 
-    className={`w-screen fixed z-30 top-0 ${isScrolled ? 'transform -translate-y-full' : ''}  transition  justify-center items-center flex bg-transparent backdrop-blur-md h-14 md:h-16 shadow-md`}>
+    <motion.main 
+    initial={{opacity: 0}}
+    animate={{opacity: 1}}
+    transition={{duration: .3}}
+    className={`w-screen opacity-0 fixed z-30 top-0 ${isScrolled ? 'transform -translate-y-full' : ''}  transition  justify-center items-center flex bg-transparent backdrop-blur-md h-14 md:h-16 shadow-md`}>
       {homeButton ? <Home /> : <Logo/>}
       {isMobile ? <Menu /> : <Nav/>}
       {!isMobile && <Socials />}
-    </main>
+    </motion.main>
   )
 }
 
