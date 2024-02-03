bbfcf40d010581a273c25f657d82b1a55af5df0b
roniekun <roniebenitez01@gmail.com>
2024-02-03 15:49:08 +0800

    update

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -61,10 +61,10 @@ const Navbar = () => {
   return (
     <nav
      ref={menu}
-     className='shadow-lg relative w-screen flex bg-[#0a0a0a] h-0 overflow-hidden'>
+     className='shadow-lg absolute w-screen flex bg-[#0a0a0a] h-0 overflow-hidden'>
         <div 
             ref={linkItems}
-          className='flex flex-col w-full mx-20  justify-center items-start text-xl h-full opacity-0'>
+          className='flex flex-col w-full mx-[5vw]  justify-center items-start text-xl h-full  opacity-0'>
             
             {/* <button
             onClick={()=>setToggleMenu(false)}
@@ -72,10 +72,10 @@ const Navbar = () => {
             <IoCloseSharp className='fill-gray-900 w-10 h-10 group-hover:opacity-90'/></button> */}
 
                 {links.map((link, index) => (
-                <div className='flex w-fit justify-center relative items-center bg-transparent p h-24 gap-5 group'>
-                <h3 className='text-md font-normal self-center text-gray-50 mx-5'>0{index+1}</h3> 
+                <div className='flex w-fit justify-center relative items-cente h-[10vh] gap-5 group'>
+                <h3 className='text-md font-normal self-center text-gray-50 mx-5 h-[10vh]'>0{index+1}</h3> 
                     <a
-                    className='text-gray-50 z-10 relative bg-transparent cursor-pointer text-4xl font-bold capitalize title-font tracking-wide flex text-balance w-fit select-none'
+                    className='text-gray-50 z-10 relative bg-transparent cursor-pointer text-[6vw] font-bold capitalize title-font tracking-wide flex text-balance w-fit select-none'
                     key={link.name} 
                     onClick={() => handleClick(link.to)}>
                      {link.name} 
