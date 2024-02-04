d25453b6c60eed7ebba3f109f83e402cc663adc1
roniekun <roniebenitez01@gmail.com>
2024-02-04 13:01:33 +0800

    update

--- src/pages/home/assets/Avatar.jsx
+++ src/pages/home/assets/Avatar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Avatar = () => {
   return (
-    <main className='flex flex-col place-items-center'>
+    <main className='flex flex-col place-items-center gap-y-2'>
       <div className='rounded-full border-2 border-gray-900 h-40 w-40 object-cover flex justify-center items-center' >
             <img src="" alt="avatar" placeholder='blur' />
           </div>
