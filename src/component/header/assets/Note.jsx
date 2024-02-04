2a68260e64c2d884646f62f0d910e540625c6707
roniekun <roniebenitez01@gmail.com>
2024-02-04 17:55:50 +0800

    update

--- src/component/header/assets/Note.jsx
+++ src/component/header/assets/Note.jsx
@@ -10,52 +10,56 @@ const Note = () => {
     const text2 = useRef(null);
 
     useEffect(() => {
-            if(!active){
+        setTimeout(() => {
+            setActive(false)
+        }, 15000);
+      
+         if(!active){
         gsap.to(container.current,{
             height: 0
         })
             }
-        setTimeout(() => {
-            setActive(false)
-        }, 50000);
     }, [active])
+
+    useEffect( () => {
 
     let xPercent = 0;
 
-useEffect( () => {
- if(slider){
-    
-  gsap.set(text1.current, {left: text2.current.getBoundingClientRect().width})
-  requestAnimationFrame(animate1);
- }
+    // animation frame
+            const animate1 = () => {
+                if (xPercent > 0) {
+                    xPercent = -100;
+                }
+                gsap.set(text1.current, { xPercent: xPercent });
+                 gsap.set(text2.current, { xPercent: xPercent });
+                requestAnimationFrame(animate1);
+                xPercent += 0.07;
+            };
 
-}, [slider])
+        if(active){
+            gsap.set(text1.current, {left: text2.current.getBoundingClientRect().width})
+            requestAnimationFrame(animate1);
+        }  
+    }, [ active ])
 
-// animation frame
-const animate1 = () => {
-  if(xPercent > 0){
-    xPercent = -100;
-  }
-
-  gsap.set(text1.current, {xPercent: xPercent})
-  gsap.set(text2.current, {xPercent: xPercent})
-  requestAnimationFrame(animate1);
-  xPercent += 0.1;
-}
 
   return (
-    <main ref={container} className='w-auto  h-14 bg-yellow-500 relative overflow-hidden'>
+    <main ref={container} className='w-auto h-10 bg-yellow-500 relative overflow-hidden'>
         <div ref={slider} className='w-full flex h-full place-items-center' >
-            <div className='flex h-auto text-nowrap text-2xl w-full'>
-                <h1 ref={text1} className='relative flex gap-x-32  px-16'>
+            <div className='flex h-auto text-nowrap text-xl w-full uppercase'>
+                <h1 ref={text1} className='relative flex gap-x-24  px-12'>
                    <span> Website is working in progress.  </span>
                    <span> Website is working in progress.  </span>
-                   <span> Website is working in progress.  </span> </h1>
+                   <span> Website is working in progress.  </span> 
+                   <span> Website is working in progress.  </span> 
+                   </h1>
                 <h1 ref={text2} 
-                className='absolute flex  gap-x-32 px-16'>
+                className='absolute flex  gap-x-24 px-12'>
                    <span> Website is working in progress.  </span> 
                    <span> Website is working in progress.  </span>
-                   <span> Website is working in progress.  </span></h1>
+                   <span> Website is working in progress.  </span>
+                   <span> Website is working in progress.  </span>
+                   </h1>
             </div>
         </div>
     </main>
